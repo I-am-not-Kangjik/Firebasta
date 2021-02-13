48b735ae1a178d7785bf2b97401800f04f0b78aa
kangjik <rkqqkb@gmail.com>
2021-02-13 16:39:47 +0900

    자체 회원관리
    자체 회원가입 및 로그인

--- Kangjiktagram/app/src/main/java/com/example/kangjiktagram/LoginActivity.kt
+++ Kangjiktagram/app/src/main/java/com/example/kangjiktagram/LoginActivity.kt
@@ -1,11 +1,61 @@
 package com.example.kangjiktagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    var auth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        findViewById<EditText>(R.id.email_edittext).setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+        findViewById<Button>(R.id.email_login_button).setOnClickListener {
+            signinAndSignup()
+        }
+    }
+
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(findViewById<EditText>(R.id.email_edittext).getText().toString(), findViewById<EditText>(R.id.password_edittext).getText().toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Creating a user account
+                    moveMainPage(task.result?.user)
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                } else {
+                    //Login if you have account
+                    signinEmail()
+                }
+            }
+    }
+
+    fun signinEmail() {
+        auth?.signInWithEmailAndPassword(findViewById<EditText>(R.id.email_edittext).getText().toString(), findViewById<EditText>(R.id.password_edittext).getText().toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Login
+                    moveMainPage(task.result?.user)
+                } else {
+                    //Show error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
